ca90ee163f9fc7e721b0bab0c368ba8f2986c926
elpisco <elpisco@gmail.com>
2016-04-01 11:34:47 -0500

    other segmentations techniques

--- proyecto/test3.py
+++ proyecto/test3.py
@@ -2,16 +2,23 @@ import numpy as np
 import SimpleITK
 import matplotlib.pyplot as plt
 from skimage.filters import sobel
+from skimage import morphology
+from skimage.segmentation import random_walker
+from scipy import ndimage as ndi
 
 
-PathDicom = "/Volumes/Files/imagenes/ALMANZA_RUIZ_JUAN_CARLOS/TAC_DE_PELVIS - 84441/_Bone_30_2/"
-outPath = "/Volumes/Files/imagenes/ALMANZA_RUIZ_JUAN_CARLOS/out/"
+PathDicom = "/home/camilo/Documents/imagenes/ALMANZA_RUIZ_JUAN_CARLOS/TAC_DE_PELVIS - 84441/_Bone_30_2/"
 
 
 def np_show(img):
     plt.figure()
     plt.imshow(img, cmap='Greys_r')
 
+
+def sitk_show(img):
+    img_array = SimpleITK.GetArrayFromImage(img)
+    plt.figure()
+    plt.imshow(img_array, cmap='Greys_r')
 
 reader = SimpleITK.ImageSeriesReader()
 filenames_dicom = reader.GetGDCMSeriesFileNames(PathDicom)
@@ -26,7 +33,17 @@ ini = 37
 end = 42
 for i in range(0, img_original.GetDepth()):
     if ini <= i <= end:
-        elevation_map = sobel(img_original[:, :, i])
-        np_show(elevation_map)
+        img_smooth = smooth_filter.Execute(img_original[:, :, i])
+        img_smooth_array = SimpleITK.GetArrayFromImage(img_smooth)
+        elevation_map = sobel(img_smooth_array)
+
+        markers = np.zeros_like(img_smooth_array)
+        markers[img_smooth_array < 100] = 1
+        markers[img_smooth_array > 300] = 2
+        segmentation = morphology.watershed(elevation_map, markers)
+        segmentation = ndi.binary_fill_holes(segmentation - 1)
+        np_show(segmentation)
+        labels = random_walker(img_smooth_array, markers, beta=10, mode='bf')
+        np_show(labels)
 
 plt.show()
