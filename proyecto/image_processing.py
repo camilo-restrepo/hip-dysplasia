823b8185d12e7a932fbf13eb97e24d8940a2a98a
elpisco <elpisco@gmail.com>
2016-05-20 13:33:30 -0500

    bla bla

--- proyecto/image_processing.py
+++ proyecto/image_processing.py
@@ -2,12 +2,12 @@ import SimpleITK
 import numpy as np
 from skimage.morphology import remove_small_objects, closing, disk, erosion, dilation
 from skimage.measure import label, regionprops
-from scipy import ndimage as ndi
 from skimage.segmentation import clear_border
+from skimage.filters import threshold_otsu
+from scipy import ndimage as ndi
 import region_growing
 import matplotlib.pyplot as plt
-from utils import Formatter
-from skimage.filters import threshold_otsu
+from skimage.color import label2rgb
 
 
 class ImageProcessing:
@@ -228,8 +228,7 @@ class ImageProcessing:
 
         return result
 
-    def get_hip(self, initial_segmentation, otsu_low_threshold):
-        femur = self.segmented_legs[self.leg_key]
+    def get_hip(self, femur, initial_segmentation, otsu_low_threshold):
         last_femur_slice = 0
         removed_elems = False
         for z in range(0, femur.shape[0]):
@@ -241,16 +240,8 @@ class ImageProcessing:
         image = initial_segmentation - femur
         image[image < 0] = 0
 
-        selem = np.array([
-            [0, 1, 1, 1, 0],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [0, 1, 1, 1, 0]
-        ])
-
-        result = np.zeros_like(otsu_low_threshold)
-        for z in range(0, otsu_low_threshold.shape[0]):
+        result = np.zeros_like(image)
+        for z in range(0, image.shape[0]):
             seed_points = set()
             white_pxs = np.argwhere(image[z, :, :] == 1)
             for px in white_pxs:
@@ -271,12 +262,24 @@ class ImageProcessing:
                 promedio = np.mean([r.area for r in regions])
 
                 for region in regions:
-                    if (region.area < promedio) or (region.centroid[1] > 200 and z > last_femur_slice and self.leg_key == self.RIGHT_LEG) or (region.centroid[1] < 50 and z > last_femur_slice and self.leg_key == self.LEFT_LEG):
+                    if (region.area < promedio) or (region.centroid[1] > 200 and z > last_femur_slice and self.leg_key == self.RIGHT_LEG) or (
+                                region.centroid[1] < 50 and z > last_femur_slice and self.leg_key == self.LEFT_LEG):
                         for coord in region.coords:
                             result[z, coord[0], coord[1]] = 0
             elif z > last_femur_slice and np.all(result[z, :, :] == 0) and not removed_elems:
                 result[z:, :, :] = 0
                 removed_elems = True
+
+        label_image = label(result)
+        for z in range(0, result.shape[0]):
+            regions = regionprops(label_image[z, :, :])
+            areas = [region.area for region in regions]
+            if len(areas) > 0:
+                max_area = np.max(areas)
+                for region in regions:
+                    if region.area != max_area:
+                        for coords in region.coords:
+                            result[z, coords[0], coords[1]] = 0
 
             result[z, :, :] = self.fill_holes(result[z, :, :], disk(1), 1)
             result[z, :, :] = remove_small_objects(result[z, :, :].astype(bool), 150)
@@ -289,11 +292,49 @@ class ImageProcessing:
             #     plt.imshow(result[z, :, :], cmap='Greys_r', interpolation="none")
             #     plt.show()
 
-        # label_image = label(result)
-        # for region in regionprops(label_image):
-        #     print region.area
-
         return result
+
+    def refine_femur_segmentation(self, otsu_low_threshold, femur):
+        last_femur_slice = 0
+        hip = self.segmented_hips[self.leg_key]
+        for z in range(0, femur.shape[0]):
+            if np.any(femur[z, :, :] == 1):
+                last_femur_slice = z
+            else:
+                break
+
+        image = otsu_low_threshold - hip
+        image[image < 0] = 0
+
+        if self.leg_key == self.LEFT_LEG:
+            for z in range(0, image.shape[0]):
+                image[z, :, :] = np.fliplr(image[z, :, :])
+
+        for z in range(0, last_femur_slice + 1):
+            image[z, 230:, :] = 0
+            image[z, :50, :] = 0
+            image[z, :, 200:] = 0
+
+        image = self.clean_by_area(image)
+        for z in range(0, last_femur_slice + 1):
+            image[z, :, :] = self.fill_holes(image[z, :, :], disk(1), 3)
+
+        if self.leg_key == self.LEFT_LEG:
+            for z in range(0, image.shape[0]):
+                image[z, :, :] = np.fliplr(image[z, :, :])
+
+        image[last_femur_slice+1:, :, :] = 0
+
+        # for z in range(0, last_femur_slice + 1):
+            # if 120 <= z <= last_femur_slice:
+            #     fig = plt.figure(z)
+            #     a = fig.add_subplot(1, 2, 1)
+            #     img = plt.imshow(otsu_low_threshold[z, :, :], cmap='Greys_r', interpolation="none")
+            #     a = fig.add_subplot(1, 2, 2)
+            #     img = plt.imshow(image[z, :, :], cmap='Greys_r', interpolation="none")
+            #     plt.show()
+
+        return image
 
 # --------------------------------------------------------------------------------------------------------------------
 # UTILS
@@ -307,6 +348,43 @@ class ImageProcessing:
         for j in range(0, iterations):
             image = erosion(image, selem)
         return image
+
+    def clean_by_area(self, binary_image):
+        image = binary_image.copy()
+        image = ndi.binary_fill_holes(image)
+
+        label_image = label(binary_image)
+        initial_label = regionprops(label_image[0, :, :])[0].label
+
+        for z in range(0, image.shape[0]):
+            regions = regionprops(label_image[z, :, :])
+            for region in regions:
+                if region.label != initial_label:
+                    for coords in region.coords:
+                        image[z, coords[0], coords[1]] = 0
+
+        for z in range(0, image.shape[0]):
+            label_image = label(image[z, :, :], connectivity=1)
+            regions = regionprops(label_image)
+            if len(regions) > 1:
+                max_area = np.max([r.area for r in regions])
+                for region in regions:
+                    if region.centroid[1] > 120 and region.area < max_area:
+                        for coords in region.coords:
+                            image[z, coords[0], coords[1]] = 0
+
+            # if 120 <= z <= 150:
+            #     image_label_overlay = label2rgb(label_image, image=image[z, :, :])
+            #
+            #     fig = plt.figure(z)
+            #     a = fig.add_subplot(1, 2, 1)
+            #     img = plt.imshow(image_label_overlay, interpolation="none")
+            #     a = fig.add_subplot(1, 2, 2)
+            #     img = plt.imshow(image[z, :, :], cmap='Greys_r', interpolation="none")
+            #     plt.show()
+
+        return image
+
 
 # --------------------------------------------------------------------------------------------------------------------
 # EXECUTION
@@ -322,5 +400,6 @@ class ImageProcessing:
             emphasized = self.get_valley_emphasized_image()
             initial_segmentation = self.initial_segmentation(emphasized)
             otsu_low_threshold = self.otsu_segmentation_low_threshold(emphasized)
-            self.segmented_legs[leg_key] = self.get_femur(initial_segmentation, otsu_low_threshold)
-            self.segmented_hips[leg_key] = self.get_hip(initial_segmentation, otsu_low_threshold)
+            femur = self.get_femur(initial_segmentation, otsu_low_threshold)
+            self.segmented_hips[leg_key] = self.get_hip(femur, initial_segmentation, otsu_low_threshold)
+            self.segmented_legs[leg_key] = self.refine_femur_segmentation(otsu_low_threshold, femur)
--- proyecto/test.py
+++ proyecto/test.py
@@ -5,15 +5,14 @@ from utils import Formatter
 
 # path = "/Volumes/Files/imagenes/ALMANZA_RUIZ_JUAN_CARLOS/TAC_DE_PELVIS - 84441/_Bone_10_4/"
 # path = "/Volumes/Files/imagenes/AVILA_MALAGON_ZULMA_IVONNE/TAC_DE_PELVIS_SIMPLE - 89589/_Bone_10_2/"
-# path = "/Volumes/Files/imagenes/ALVAREZ_PATINO_SOFIA/PELVIS - 73864/_Bone_10_5/"
+path = "/Volumes/Files/imagenes/ALVAREZ_PATINO_SOFIA/PELVIS - 73864/_Bone_10_5/"
 # path = "/Volumes/Files/imagenes/CHACON_BARBA_SERGIO_ANDRES/CADERA_SIMPLE - 103000/_Bone_10_4/"
-path = "/Volumes/Files/imagenes/LOAIZA_ORTIZ_JONATHAN_ESTEVEN/CADERA_IZQUIERDA - 94647/_Bone_10_5/"
+# path = "/Volumes/Files/imagenes/LOAIZA_ORTIZ_JONATHAN_ESTEVEN/CADERA_IZQUIERDA - 94647/_Bone_10_5/"
 # path = "/Volumes/Files/imagenes/VARGAS_DIAZ_CLAUDIA_PATRICIA/PELVIS_3D - 84024/_Bone_10_5/"
 
 c = ImageProcessing()
 c.execute(path)
 legs = c.legs
-emphasized = c.emphasized
 segmented_legs = c.segmented_legs
 segmented_hips = c.segmented_hips
 
@@ -21,12 +20,9 @@ for leg_key in legs.keys():
 
     for k in range(100, legs[leg_key].shape[0]):
         fig = plt.figure(k)
-        a = fig.add_subplot(1, 3, 1)
-        img = plt.imshow(emphasized[leg_key][k, :, :], cmap='Greys_r', interpolation="none")
-        a.format_coord = Formatter(emphasized[leg_key][k, :, :])
-        a = fig.add_subplot(1, 3, 2)
+        a = fig.add_subplot(1, 2, 1)
         img = plt.imshow(segmented_legs[leg_key][k, :, :], cmap='Greys_r', interpolation="none")
-        a = fig.add_subplot(1, 3, 3)
+        a = fig.add_subplot(1, 2, 2)
         img = plt.imshow(segmented_hips[leg_key][k, :, :], cmap='Greys_r', interpolation="none")
         if k % 20 == 0:
             plt.show()
